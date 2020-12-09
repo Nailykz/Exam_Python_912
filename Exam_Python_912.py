eff9d14d78515646c7c2f9b1aba21beb75417e92
ZINS Kilian <kilian.zins53000@gmail.com>
2020-12-09 15:09:00 +0100

    Update_Ajout_Regles

--- Exam_Python_912.py
+++ Exam_Python_912.py
@@ -3,13 +3,13 @@ from colorama import init
 from colorama import Fore, Back, Style
 import random 
 
-def Tour_Du_Joueur(Mot_A_Trouver)
+def Tour_Du_Joueur(Mot_A_Trouver):
     Proposition_Du_Joueur = input("Proposez un mot : ")
-    if(len(Proposition_Du_Joueur) > 6:
+    if(len(Proposition_Du_Joueur)) > 6:
         print("Le mot est trop grand.")
-    else:
+    if(len(Proposition_Du_Joueur)) < 6 :
         print("Le mot est trop petit.")
-    else if:
+    else:
         for i in range(0,6):
             if(Proposition_Du_Joueur[i] == Mot_A_Trouver[i]):
                 print(Back.RED + Proposition_Du_Joueur[i],end="")
@@ -17,4 +17,22 @@ def Tour_Du_Joueur(Mot_A_Trouver)
                 print(Back.BLUE + Proposition_Du_Joueur[i],end="")
         return Proposition_Du_Joueur
 
+def Test_Victoire(Proposition_Du_Joueur, Mot_A_Trouver):
+        if(Proposition_Du_Joueur == Mot_A_Trouver):
+            Victoire == True
+        else :
+            Victoire == False
+        return Victoire
+        
+def Explication_Motus():
+        print("Dans motus, il faut rechercher un mots d'un nombre fixé de lettres.\n")
+        print("Le mot doit contenir le bon nombre de lettres, sinon il est refusé.\n")
+        print("Les lettres ont un code couleur : ")
+        print("Une lettre bien placée sera indiquée en rouge.\n")
+        print("Une lettre mal placée mais présente dans le mot sera indiquée en jaune.\n")
+        print("Une lettre ne se trouvant pas dans le mot sera indiquée en bleu.\n")
+        print("Vous avez 8 tentatives.", end="")
+
+Explication_Motus()
+        
 input()       
